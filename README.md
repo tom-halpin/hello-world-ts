664e9ff6923365d6cea768372266ca9aea2c08e6
Tom Halpin <tom.halpin@dxc.com>
2021-03-20 15:07:02 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# hello-world-ts
-Hello World TypeScript
+# READM.md
+
+Hello World with some minor bells and whistles in TypeScript
+
+## Setup
+
+```console
+npm install -g TypeScript
+```
+
+```console
+npm install inquirer
+```
+
+```console
+npm install chalk
+```
+
+## Compile
+
+Was originally compiling using following command line
+
+```console
+tsc hello-world.tsc
+```
+
+but got following error after importing ```chalk```
+
+added ```"esModuleInterop": true``` to ```tsconfig.json``` to fix as per Mr Google
+
+now compiling using
+
+```console
+tsc
+```
+
+
+## Run
+
+```console
+node hello-world
+```
---
+++ hello-world.ts
@@ -0,0 +1,46 @@
+import { Answers, prompt, InputStreamOption, Question, Questions } from 'inquirer';
+import chalk from 'chalk';
+
+//define class with constructor
+const questions: Question[] = [
+    {
+      type: 'input',
+      name: 'name',
+      message: chalk.green('Enter your name [required]'),
+      validate: (value: any) => {
+        if (!value) {
+          return chalk.red('Please enter your name');
+        } else if (!/^[a-zA-Z\s]*$/.test(value)) {
+          return chalk.red('Name must be contain only letters and spaces.');
+        }
+        return true;
+      },
+    },
+    {
+      type: 'list',
+      name: 'message',
+      message: chalk.green('Select message to display [required]'),
+      choices: ['Hello', 'Hi', "Howdy", "Good day", "Dia dhuit"],
+    },
+  ];
+
+class HelloWorld {
+    private prompt: Promise<Answers> | undefined = undefined;
+    constructor() {
+        console.log('world building');
+    }
+
+    public run() {
+        console.log('Firing up the flux capacitor');
+        this.prompt = prompt(questions);
+        this.prompt.then((answers: Answers) => {
+            console.log('-------------------------------');
+            console.log(chalk.blueBright(answers.message), chalk.blueBright(answers.name));
+        });
+
+    }
+}
+
+const helloWorld = new HelloWorld();
+
+helloWorld.run();
